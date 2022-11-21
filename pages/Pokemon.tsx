58f0cc296fa312efee2f425dc8498981a41c654f
AlexandreLefay <alexandre.lefay@gmail.com>
2022-11-21 20:58:40 +0100

    previous + next and sprite

--- pages/Pokemon.tsx
+++ pages/Pokemon.tsx
@@ -1,95 +1,58 @@
-// import React, { Fragment, useState, useEffect } from "react"
-// import axios from "axios"
-
-// export default function Pokemon() {
-//   const [pokemon, setPokemon] = useState([])
-
-//   useEffect(() => {
-//     axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-//       setPokemon(res.data.results.map((p) => p.name))
-//     })
-//   }, [])
-
-//   return (
-//     <div>
-//       {pokemon.map((p) => (
-//         <div key={p}>{p}</div>
-//       ))}
-//     </div>
-//   )
-// }
-
-import React, { Fragment, useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-export default function Pokemon() {
-  const [pokemon, setPokemon] = useState([])
-  const [pokemonType, setPokemonType] = useState([])
-  const [pokemonData, setPokemonData] = useState([])
+export default function PokeAPI() {
+  const [name, setName] = useState("")
+  const [PokemonId, setPokemonId] = useState(1)
+  const [Find, setFind] = useState("")
+  const [Img, setImg] = useState("")
+  const [Type, setType] = useState("")
 
-  const id = pokemon.length + 1
-
-  // hook similaire à "componentDidMount" et "componentDidUpdate"
-  //Récupération de 151 pokemon; nom :
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => {
-      setPokemon(res.data.results.map((p: { name: any }) => p.name))
-    })
-  }, [])
-  console.log(pokemon)
+    async function getData() {
+      let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${PokemonId}`)
+      console.log(res)
+      setPokemonId(res.data.id)
+      setFind(res.data.name)
+      setImg(res.data.sprites.front_default)
+      setType(res.data.types[0].type.name)
+    }
 
-  // Récupération des types de chaque pokemon
-  // useEffect(() => {
-  //   axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => {
-  //     setPokemonType(res.data.results.map((p: { abilities: any }) => p.abilities))
-  //   })
-  // }, [])
-  // console.log(pokemonType)
+    getData()
+  }, [PokemonId])
+  console.log(PokemonId + "Id pokemon")
+  const Typename = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setName(event.target.value)
+  }
+
+  const Search = () => {
+    if (name !== "") setFind(name)
+    setName("")
+  }
+
+  const handleNext = () => setPokemonId(PokemonId + 1)
+  const handlePrevious = () => setPokemonId(PokemonId - 1)
 
   return (
-    <Fragment>
-      <ul>
-        {pokemon.map((p, id) => (
-          <li key={id + 1}>
-            <p>
-              {p}, {id + 1}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </Fragment>
+    <>
+      <div className="back">
+        <div className="card">
+          <img src={`${Img}`} alt="" />
+          <div className="name">
+            {Find.toUpperCase()} N°{PokemonId}
+          </div>
+
+          <div className="type">{Type}</div>
+
+          <input type="text" onChange={Typename} value={name} />
+
+          <button>Search</button>
+        </div>
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+
+      <div>Pokedex</div>
+    </>
   )
 }
-
-// const result = await axios("https://jsonplaceholder.typicode.com/posts")
-
-// import React, { Fragment, useState, useEffect } from "react"
-// import axios from "axios"
-
-// export default function Pokemon() {
-//   const [data, setData] = useState([])
-
-//   // hook similaire à "componentDidMount" et "componentDidUpdate"
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const result = await axios("https://pokeapi.co/api/v2/pokemon?limit?151")
-//       setData(res.data.results.map((p) => p.name))
-//       console.log("updated")
-//     }
-//     fetchData()
-//     console.log("mounted")
-//   }, [])
-//   console.log(data)
-
-//   return (
-//     <Fragment>
-//       <ul>
-//         {data.map((item) => (
-//           <li key={item.id}>
-//             <p>{item.id}</p>
-//           </li>
-//         ))}
-//       </ul>
-//     </Fragment>
-//   )
-// }
